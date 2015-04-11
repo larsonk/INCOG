3cfd77df6a4a57977d556cbd88e134b2a015cbb5
larsonk <larsonk@terpmail.umd.edu>
2015-04-11 16:11:27 -0400

    icon

--- INCOG/app/src/main/java/com/example/katherinelarson/incog/InstaAuth.java
+++ INCOG/app/src/main/java/com/example/katherinelarson/incog/InstaAuth.java
@@ -1,7 +1,15 @@
 package com.example.katherinelarson.incog;
 
+
 /**
  * Created by Katherine Larson on 4/11/2015.
  */
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+
 public class InstaAuth {
+    public static final String client_id = "aedc75e349b04c238fd3f5e43fe2730b";
+    public static final String client_secret = "0c477b99b1654d51a89775e4b1bf5658";
+
+
 }
